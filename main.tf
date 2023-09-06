f6afc89ace735c0d26794957bfa0ace0d470d8b9
Luke Czoboriek <lc169782@protonmail.com>
2023-09-06 15:41:57 +0000

    Added in backups of destroyed volumes

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-locals { # Terraform Locals are named values which can be assigned and used in your code. It mainly serves the purpose of reducing duplication within the Terraform code.
-  deployment = {
-    nodered = {
-      container_count = length(var.ext_port["nodered"][terraform.workspace])
-      image = var.image["nodered"][terraform.workspace]
-      int   = 1880
-      ext   = var.ext_port["nodered"][terraform.workspace] # So we access the value by specifying its a variable, the ext_port variable, and the value is located within
-      # the map of node red, and specifically the workspace we are working within. 
-      container_path = "/data"
-    }
-    influxdb = {
-      container_count = length(var.ext_port["influxdb"][terraform.workspace])
-      image          = var.image["influxdb"][terraform.workspace]
-      int            = 8086
-      ext            = var.ext_port["influxdb"][terraform.workspace]
-      container_path = "/var/lib/influxdb"
-    }
-    graffana = {
-      container_count = length(var.ext_port["graffana"][terraform.workspace]) # This will define the port based on the workspace and the app_container
-      image = var.image["graffana"][terraform.workspace]
-      int = 3000
-      ext = var.ext_port["graffana"][terraform.workspace]
-      container_path = "/var/lib/graffana"
-    }
-  }
-}
 
 
 # Graffana task
